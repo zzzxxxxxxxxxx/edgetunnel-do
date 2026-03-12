7449317c28c323d13066740c6c6f1a1ade4aa444
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-03-13 01:25:11 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -18,7 +18,6 @@ const DOH_SERVERS = [
 	{ wire: 'https://1.0.0.1/dns-query',        json: 'https://1.0.0.1/dns-query' },        // Cloudflare secondary
 	{ wire: 'https://8.8.8.8/dns-query',        json: 'https://8.8.8.8/resolve' },          // Google
 	{ wire: 'https://8.8.4.4/dns-query',        json: 'https://8.8.4.4/resolve' },          // Google secondary
-	{ wire: 'https://9.9.9.9/dns-query',        json: 'https://9.9.9.9/dns-query' },        // Quad9
 ];
 let dohIndex = 0;
 
